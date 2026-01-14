d1d3fce369d6fedf5120d85f1ef2553b41f85f1a
bao84699 <bao84699@gmail.com>
2026-01-14 23:27:08 +0700

    v09

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ let sections = document.querySelectorAll(".section2, .section3, .section4");
 let nowsize = 0.7;
 
 function updateTrigger() {
-    if (window.innerWidth < 800) {
-        nowsize = 0.9; // mobile
-    } else {
-        nowsize = 0.7; // desktop
-    }
+    nowsize = window.innerWidth < 800 ? 0.9 : 0.7;
 }
-
+updateTrigger();
+window.addEventListener("resize", updateTrigger);
 window.addEventListener("scroll", () => {
     const scrolling = window.scrollY + window.innerHeight * nowsize;
     if (scrolling >= section1.offsetTop) {
         section1.classList.add("show");
-        section1a.classList.add("flight");
+        if (!section1a.classList.contains("flight")) {
+            section1a.classList.add("flight");
+        }
     } else {
         section1.classList.remove("show");
         section1a.classList.remove("flight");
@@ -28,5 +27,4 @@ window.addEventListener("scroll", () => {
             section.classList.remove("show");
         }
     });
-
 });
